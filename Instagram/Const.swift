18aa9c1114aaa1a2d0a2de95c662158d7be1664c
asaku235 <shimoyama626@gmail.com>
2019-07-24 12:25:15 -0400

    コメント表示のラベルを追加

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Taka on 7/22/19.
+//  Copyright © 2019 Taka. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
